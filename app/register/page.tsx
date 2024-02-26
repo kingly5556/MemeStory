583422c566425fdf867f5dfc316e0b5fbc2a53ac
kingly5556 <kingly5556@gmail.com>
2024-02-26 19:47:07 +0700

    chobheeFirstComit

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,38 @@
+
+import { createClient } from '@/utils/supabase/server';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-export default function page() {
+export default function Login({
+  searchParams,
+}: {
+  searchParams: { message: string };
+}){
+  const signUp = async (formData: FormData) => {
+    "use server";
+  
+    const origin = headers().get("origin");
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    const supabase = createClient();
+  
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${origin}/auth/callback`,
+      },
+    });
+  
+    if (error) {
+      return redirect("/login?message=Could not authenticate user");
+    }
+  
+    return redirect("/login?message=Check email to continue sign in process");
+  };
   return (
     <div>register page</div>
   )
 }
+
