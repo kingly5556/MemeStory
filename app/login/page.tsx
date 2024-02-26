5718a56c9ac90f3d7a0a390963bcb40593141e8d
Siwakorn.Phum <150316488+siwakorn1123@users.noreply.github.com>
2024-02-26 20:26:45 +0700

    register0.2

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -28,29 +28,6 @@ export default function Login({
     return redirect("/protected");
   };
 
-  const signUp = async (formData: FormData) => {
-    "use server";
-
-    const origin = headers().get("origin");
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    const supabase = createClient();
-
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: `${origin}/auth/callback`,
-      },
-    });
-
-    if (error) {
-      return redirect("/login?message=Could not authenticate user");
-    }
-
-    return redirect("/login?message=Check email to continue sign in process");
-  };
-
   return (
     <div className=" py-20 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
       <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
@@ -79,13 +56,6 @@ export default function Login({
           pendingText="Signing In..."
         >
           Sign In
-        </SubmitButton>
-        <SubmitButton
-          formAction={signUp}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing Up..."
-        >
-          Sign Up
         </SubmitButton>
         {searchParams?.message && (
           <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,75 @@
+import { createClient } from '@/utils/supabase/server';
+import { headers } from 'next/headers';
 import React from 'react'
+import { SubmitButton } from '../login/submit-button';
+import { redirect } from 'next/navigation';
 
-export default function page() {
+export default function page({
+  searchParams,
+}: {
+  searchParams: { message: string };
+}) {
+
+  const signUp = async (formData: FormData) => {
+    "use server";
+
+    const origin = headers().get("origin");
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    const supabase = createClient();
+
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${origin}/auth/callback`,
+      },
+    });
+
+    if (error) {
+      return redirect("/login?message=Could not authenticate user");
+    }
+
+    return redirect("/login?message=Check email to continue sign in process");
+  };
+
   return (
-    <div>register page</div>
+
+    <div className=" py-20 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+      <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
+        <label className="text-md" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          name="email"
+          placeholder="you@example.com"
+          required
+        />
+        <label className="text-md" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          type="password"
+          name="password"
+          placeholder="••••••••"
+          required
+        />
+      <SubmitButton
+        formAction={signUp}
+        className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
+        pendingText="Signing Up..."
+        >
+        Sign Up
+    </SubmitButton>
+    {searchParams?.message && (
+          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
+            {searchParams.message}
+          </p>
+        )}
+      </form>
+    </div>
   )
 }
+
