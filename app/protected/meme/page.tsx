62ba7bb7d6b93eb2fe134d4e107b79ba1f0250e0
kingly5556 <kingly5556@gmail.com>
2024-03-02 22:26:39 +0700

    This Progress So Fucking Badass

--- app/protected/meme/page.tsx
+++ app/protected/meme/page.tsx
@@ -1,18 +1,20 @@
-import { createClient } from '@/utils/supabase/server';
-import { redirect } from 'next/navigation';
-import React from 'react'
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import React, { useState } from "react";
 
 export default async function ProtectedPage() {
-    const supabase = createClient();
+  const supabase = createClient();
+  // const [users, setUser] = useState<any>([]);
+  // const fetchData = async () => {
+  //   let { data, error } = await supabase.from("post").select("*");
+  // };
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/login");
+  }
   
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-  
-    if (!user) {
-      return redirect("/login");
-    }
-  return (
-    <div>meme page</div>
-  )
+  return <div>meme page</div>;
 }
--- app/protected/postmeme/page.tsx
+++ app/protected/postmeme/page.tsx
@@ -1,69 +1,71 @@
-import React from "react";
-import { SubmitButton } from "../login/submit-button";
 import { createClient } from "@/utils/supabase/server";
-import { headers } from "next/headers";
+import React from "react";
+import { SubmitButton } from "@/app/login/submit-button";
+import { redirect } from "next/navigation";
+import { v4 } from "uuid";
 
 export default function page({
   searchParams,
 }: {
   searchParams: { message: string };
 }) {
-  const signUp = async (formData: FormData) => {
+  const post = async (formData: FormData) => {
     "use server";
-    const origin = headers().get("origin");
-    const email = formData.get("email")as string;
-    const password = formData.get("password") as string;
     const supabase = createClient();
-    
-    const {  } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: `${origin}/auth/callback`,
-      },
-    });
-    
+    const name = formData.get("name") as string;
+    const pic = formData.get("pic") as File;
+    const filename = v4();
+    const publicAttachmentUrl = supabase.storage
+        .from("attachments").getPublicUrl(filename);
     const { data, error } = await supabase
-      .from("user")
+      .from("post")
       .insert([{ 
-        email,
-        password
+        name,
+        pic : publicAttachmentUrl
       }]);
     
     if(error){
       console.log(error)
       return false;
     }
-  };
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
 
+    if (!user) {
+      return redirect("/login");
+    }
+    const {} = await supabase.storage
+      .from("attachments")
+      .upload(filename, pic);
+    };
   return (
     <div className=" py-20 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
       <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
-        <label className="text-md" htmlFor="email">
-          Email
+      <label className="text-md" htmlFor="name">
+          Meme name
         </label>
-        <input
+      <input
           className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
-          placeholder="you@example.com"
+          name="name"
+          placeholder="Name of meme"
           required
         />
-        <label className="text-md" htmlFor="password">
-          Password
+        <label className="text-md" htmlFor="pic">
+          Meme Picture
         </label>
         <input
           className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          type="password"
-          name="password"
-          placeholder="••••••••"
+          type="file"
+          name="pic"
           required
         />
         <SubmitButton
-          formAction={signUp}
+          formAction={post}
           className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
           pendingText="Signing Up..."
         >
-          Sign Up
+            Upload This Post
         </SubmitButton>
         {searchParams?.message && (
           <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
